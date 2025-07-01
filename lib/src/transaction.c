ec871523f5f392c878aa8d7eb2d5383e3bf3d05c
npc1054657282 <ly1054657282@gmail.com>
2025-07-01 23:29:03 +0800

    fix: `strdup` not free In `neo4j_config_set_supported_versions` and `new_transaction`, the `strdup`ed vals never free. The especially special one is `tx->mode = ( mode == NULL ? "w" : strdup(mode) )` The ownership of the memory for `tx->mode` is very ambiguous. It's a headache to release it. So I change this to be only static "w" or static "s". If the input `mode` is not equal to "s", then `tx->mode` will be default "w". The `tx->dbname` also has an issue. Though it is defined as `const char *`, which means the tx does not own the string object. However, it is created with `strdup`, which means it owns the string and should free it. So I believe its type is incorrect. An alternative option is to let the dbname just refer the arg `dbname`, which require the caller to ensure the lifetime of the `dbname`. I do not like this. I choose to modify the `tx->mode` to `char *`.

--- lib/src/transaction.c
+++ lib/src/transaction.c
@@ -375,7 +375,7 @@ neo4j_transaction_t *new_transaction(neo4j_config_t *config, neo4j_connection_t 
   tx->run = tx_run;
 
   tx->timeout = timeout;
-  tx->mode = ( mode == NULL ? "w" : strdup(mode) );
+  tx->mode = ( (mode == NULL || strcmp(mode, "r")) ? "w" : "r" );
   tx->dbname = strdup(dbname);
   tx->is_open = 0;
   tx->is_expired = 0;
@@ -535,6 +535,7 @@ void neo4j_free_tx(neo4j_transaction_t *tx)
   if (tx->num_bookmarks > 0) {
     neo4j_free(tx->allocator, (void *)tx->bookmarks);
   }
+  free(tx->dbname);
   neo4j_mpool_drain(&tx->mpool);
   neo4j_logger_release(tx->logger);
   // free tx space
